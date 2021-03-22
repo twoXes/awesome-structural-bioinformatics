8ca2c762362311ea4dd0db89ea6297b6a587e320
John Doe <johndoe@example.com>
2021-03-22 12:51:49 +0000

    started streamlit

--- app.py
+++ app.py
@@ -1,3 +1,48 @@
 import streamlit as st
+import Bio 
 
-st.title('Learning Molecular Informatics Artificially Deep')
+'''
+
+#  Molecular Informatics Artifically Deep
+
+An artifically deep dive into machine learning methods for molecular informatics 
+
+'''
+
+st.image("chemistry.gif")
+
+'''
+Most code written in Python :dragon:  except when it isn't. 
+
+Want a better way to learn? Spend 4-12 years of your life and hundreds of thousands of dollars chasing a paper with a stamp on it :medal:
+
+Or feed yourself :baby_bottle:
+
+## Why This Book
+
+Most books on Cheminformatics, Bioinformatics, Quantum Chemistry, are good for inducing sleep 😴 and  fleecing your wallet :moneybag: 
+
+Knoweldge should be cheap, fast enjoyable, silly, shared, disproven, contested, fun and most of all free. This book combats knowledge hodlers, and innovation stifflers by not being boring and old. 
+
+Though we look at  supposedly outdated (if you can't date yourself, who can you date?) Machine Learning methods as applied to Molecular Informatics, this is for the young of mind and young of spirit 🚼 
+
+## Other Free Books You Should Read Instead of This
+
+[Chemisty 2E](https://openstax.org/details/books/chemistry-2e) - :atom_symbol: Equivalent to 201 & 202 Level Chemistry Book
+
+[Chemistry: Atoms First 2E](https://openstax.org/details/books/chemistry-atoms-first-2e) :atom_symbol: Fork of 2E but not with more Atoms!!!!
+
+[Biology 2E](https://openstax.org/details/books/biology-2e) 👽 Like Chemistry 2E but Biology 
+
+[Artificial Intelligence: A Modern Approach]( https://github.com/aimacode/aima-python) 🤖 The Gospel of Machine Learning
+
+[Neural Networks and Deep Learning](http://neuralnetworksanddeeplearning.com/) 🤖 Michael Nielsen writes another masterpiece - About Deep Learning - if you are into that sort of thing. 
+
+[Reinforcement Learning](http://incompleteideas.net/book/RLbook2020.pdf) 🤖  The only book you need on the subject 
+
+[Pattern Recognition and Machine Learning](https://www.microsoft.com/en-us/research/uploads/prod/2006/01/Bishop-Pattern-Recognition-and-Machine-Learning-2006.pdf) 🤖 Another classic banger
+
+Structural Classification of Proteins [Database](http://scop.mrc-lmb.cam.ac.uk/)
+
+'''
+
